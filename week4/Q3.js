64901633ebda2fd3f357d906179c485e200df63f
Alexis Pendleton <59650617+LittleMissButtons@users.noreply.github.com>
2022-10-14 08:54:26 +0100

    Update Q3.js
    comments added and changed 'dog' object to let instead of const

--- week4/Q3.js
+++ week4/Q3.js
@@ -1,7 +1,7 @@
-const dog = {
+let dog = { //age will change so used 'let' instead of const
     name: "Sappho",
-    age: 1,
-    type: "Labradoodle"
+    age: 1.2, //float number for more accurate age 
+    type: "Labradoodle" //bread of dog
 };
 
 console.log(dog); //dog details
