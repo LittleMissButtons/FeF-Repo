71adb0f6a89eff00a719b311d7035c44c5b74c33
Alexis Pendleton <59650617+LittleMissButtons@users.noreply.github.com>
2022-10-14 22:05:37 +0100

    Update Q4.js

---
+++ week4/Q4.js
@@ -0,0 +1,14 @@
+let dog = { //age will change so used 'let' instead of const
+    name: "Sappho",
+    age: 1.2, //float number for more accurate age 
+};
+
+let human = { //age will change so used 'let' instead of const
+    name: "Alexis",
+    age: 39, //float number for more accurate age 
+};
+
+//add if else statement
+//if human then age * 7; else age / 7
+
+console.log(age); //dog details
