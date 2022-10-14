d93d081e2ea97c7fbe8ab89d6c0145097f0fcec2
Alexis Pendleton <59650617+LittleMissButtons@users.noreply.github.com>
2022-10-14 21:49:54 +0100

    draft Q2 code

--- week4/Q2.js
+++ week4/Q2.js
@@ -1,20 +1,15 @@
-
-let address = { //address object
-    streetName: "Lady Village",
-    houseNr: "Roadside Cottage",
-    postCode: "KW17 2BW",
-    city: "Orkney Islands",
-    country: "Scotland"
-};
-
-let favThings = ["music", "space", "family time", "pets", "walking on the beach"]; //favorite things array
-
-let person = { //person object
+const person = { //person object
     name: "Alexis",
     type: "Human",
     age: 39,
-    address: address, //full address taken from 'address' object
-    favThings: favThings[0, 1, 2] //top 3 favorite things from array
+    address: { //full address taken from 'address' object
+        streetName: "Lady Village",
+        houseNr: "Roadside Cottage",
+        postCode: "KW17 2BW",
+        city: "Orkney Islands",
+        country: "Scotland"
+    },
+        favThings : ["music", "space", "family time", "pets", "walking on the beach"] //top 5 favorite things from array
 };
 
 console.log(person); //persons details
